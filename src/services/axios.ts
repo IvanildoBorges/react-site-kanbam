08926b311c92bb8ec2bd0f2fef704000a958ad13
IvanildoBorges <ivanildo.rap@gmail.com>
2023-03-15 17:36:20 -0300

    Alterado a variavel api para remoto

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const backend: string = "https://api-site-kanban-deploy.onrender.com"; //  http://localhost:3333
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: backend
 })
