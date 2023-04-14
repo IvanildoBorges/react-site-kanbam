50a32b46150c63c033022ab1ce9888ec1d1458dc
IvanildoBorges <ivanildo.rap@gmail.com>
2023-04-14 11:29:43 -0300

    Alterado constante do service

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const backend: string = "https://api-site-kanban-deploy.onrender.com"; //  http://localhost:3333
+const backend: string = "https://api-site-kanban.onrender.com";
+//const backend: string = "http://localhost:3333";
 
 export const api = axios.create({
   baseURL: backend
